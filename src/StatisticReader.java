8d6a19402ac3308be4a5228d56f2f050d927eb98
joausaga <jorgesaldivar@gmail.com>
2013-10-02 16:22:01 +0200

    Add a statement to replace quote symbol with a blank space

--- src/StatisticReader.java
+++ src/StatisticReader.java
@@ -20,26 +20,26 @@ public class StatisticReader extends HTMLReader {
 	{
 		HashMap<String,String> statistics = new HashMap<String,String>();
 		statistics.put("ideas", "unknown");
-    	statistics.put("comments", "unknown");
-    	statistics.put("votes", "unknown");
-    	statistics.put("members", "unknown");
+		statistics.put("comments", "unknown");
+		statistics.put("votes", "unknown");
+		statistics.put("members", "unknown");
 		
 		String content;
 		try {
 			content = getUrlContent(url);
-			 Document doc = Jsoup.parse(content);
+			Document doc = Jsoup.parse(content);
 		        
-	        Element ideasStats = doc.getElementById(IDEAS_STATS).child(0);
-	        if (ideasStats != null)
-	        	statistics.put("ideas", ideasStats.text());
+			Element ideasStats = doc.getElementById(IDEAS_STATS).child(0);
+			if (ideasStats != null)
+			    statistics.put("ideas", replaceThounsandSymbol(ideasStats.text()));
 	        	
-	        Elements otherStats = doc.getElementById(OTHER_STATS).children();
-	        if (otherStats != null) {
-	        	statistics.put("comments", otherStats.get(0).text());
-	        	statistics.put("votes", otherStats.get(1).text());
-	        	statistics.put("members", otherStats.get(2).text());
-	        }
-	        return statistics;
+			Elements otherStats = doc.getElementById(OTHER_STATS).children();
+			if (otherStats != null) {
+			    statistics.put("comments", replaceThounsandSymbol(otherStats.get(0).text()));
+			    statistics.put("votes",replaceThounsandSymbol(otherStats.get(1).text()));
+			    statistics.put("members", replaceThounsandSymbol(otherStats.get(2).text()));
+			}
+			return statistics;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			return statistics;
@@ -48,5 +48,9 @@ public class StatisticReader extends HTMLReader {
         
         
 	}
+
+    private String replaceThounsandSymbol(String str) {
+	return str.replace("K","000");
+    }
 	
 }
