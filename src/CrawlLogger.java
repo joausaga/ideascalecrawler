970e5334cf4932ee7fa705c7b2f9c5d43749353c
joausaga <jorgesaldivar@gmail.com>
2014-03-11 11:27:51 +0100

    Change in the name of the log file

--- src/CrawlLogger.java
+++ src/CrawlLogger.java
@@ -1,6 +1,10 @@
 package src;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,12 +16,15 @@ public class CrawlLogger {
 	static private SimpleFormatter formatterTxt;
 
 	static public void setup() throws IOException {
-
+		Calendar cal = Calendar.getInstance();
+		Date today = cal.getTime();
+		DateFormat timeFormat = new SimpleDateFormat("dd-MM-yyyy");
+		
 		// Get the global logger to configure it
 		Logger logger = Logger.getLogger("");
 
 		logger.setLevel(Level.INFO);
-		fileTxt = new FileHandler("logs/crawler.txt");
+		fileTxt = new FileHandler("logs/crawler_"+timeFormat.format(today)+".txt");
 
 		// create txt Formatter
 		formatterTxt = new SimpleFormatter();
